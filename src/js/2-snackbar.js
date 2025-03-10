f023be840d8d41195ab08e3e21eb4b7e438da349
Mary <maryhoglan78@gmail.com>
2025-03-10 17:02:10 +0200

    js files

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,36 +1,51 @@
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 
-document.querySelector(".form").addEventListener("submit", (event) => {
-    event.preventDefault();
+iziToast.settings({
+    position: 'topRight',  
+    timeout: 5000,         
+    closeOnEscape: true,  
+    progressBar: true,     
+    theme: 'dark',        
+    transitionIn: 'fadeIn', 
+    transitionOut: 'fadeOut', 
+    pauseOnHover: true,
+    balloon: true,
+    icon: 'fa fa-info-circle',
+});
 
-    const form = event.target;
-    const delay = Number(form.delay.value);
-    const state = form.state.value;
 
-    createPromise(delay, state)
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.querySelector(".form");
+
+    form.addEventListener("submit", (event) => {
+        event.preventDefault();
+
+        const delay = Number(form.elements.delay.value);
+        const state = form.elements.state.value;
+
+      
+
+   
+
+        new Promise((resolve, reject) => {
+            setTimeout(() => {
+                state === "fulfilled" ? resolve(delay) : reject(delay);
+            }, delay);
+        })
         .then((delay) => {
             iziToast.success({
-                title: "✅ Success",
-                message: `Fulfilled promise in ${delay}ms`,
-                position: "topRight",
+                title: "Success",
+                message: `✅ Fulfilled promise in ${delay}ms`,
             });
         })
         .catch((delay) => {
             iziToast.error({
-                title: "❌ Error",
-                message: `Rejected promise in ${delay}ms`,
-                position: "topRight",
+                title: "Error",
+                message: `❌ Rejected promise in ${delay}ms`,
             });
         });
-
-    form.reset();
-});
-
-function createPromise(delay, state) {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            state === "fulfilled" ? resolve(delay) : reject(delay);
-        }, delay);
     });
-}
+});
